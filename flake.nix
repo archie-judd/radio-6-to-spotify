a4fb1826e07f4067ea26213823912e616706617e
archiejudd <archie.judd@yahoo.com>
2024-07-31 21:45:36 +0100

    update flake (2)

--- flake.nix
+++ flake.nix
@@ -1,7 +1,10 @@
 {
   inputs = {
+
     flake-utils.url = "github:numtide/flake-utils";
+
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
+
     poetry2nix = {
       url = "github:nix-community/poetry2nix";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -12,15 +15,15 @@
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
+
         _poetry2nix = poetry2nix.lib.mkPoetry2Nix { inherit pkgs; };
+
         overrides = _poetry2nix.defaultPoetryOverrides.extend (self: super: {
           bs4 = super.bs4.overridePythonAttrs (old: {
             buildInputs = (old.buildInputs or [ ]) ++ [ super.setuptools ];
           });
-          matplotlib = super.matplotlib.overridePythonAttrs (old: {
-            buildInputs = (old.buildInputs or [ ]) ++ [ super.meson ];
-          });
         });
+
         myEnv = _poetry2nix.mkPoetryEnv {
           projectDir = ./.;
           editablePackageSources = {
@@ -28,15 +31,18 @@
           };
           overrides = overrides;
         };
+
         myApp = _poetry2nix.mkPoetryApplication {
           projectDir = ./.;
           overrides = overrides;
         };
+
       in {
         devShells.default = pkgs.mkShell {
           inputsFrom = [ myEnv.env ];
           packages = [ pkgs.poetry ];
         };
+
         apps.default = {
           type = "app";
           program = "${myApp}/bin/sync_playlists";
